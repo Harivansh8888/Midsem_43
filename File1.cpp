cd3c66639cad09f6f258bddbf2e6f132534e4bf6
Harivansh Mathur <mathurharivansh811@gmail.com>
2019-03-18 10:48:57 +0530

    File1,File2,File3---First Commit

---
+++ File1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+class Test
+{
+	public:
+		void details
+		{
+			string name;
+			cout<<"Hello"<<endl;
+			cout<<"Enter Your Name"<<endl;
+			cin>>name;
+			cout<<"Hii"<<" "<<name<,endl;//Printing a Greeting//
+		}
+};
+int main()
+{
+	Test t1;
+	t1.details();
+	return 0;
+}
+
